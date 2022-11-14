44a5c2153ce8bc3e0ae64269c6cf87eb25508e13
Diana C Peterson <dpeter10@gmail.com>
2022-11-14 11:43:14 -0500

    commit tree 062c5835c5808582564d060a1b8cb6b0c321cb6d

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+const imgDiv = document.getElementById('dog-image-container');
+const breedUl = document.getElementById('dog-breeds');
+fetch(imgUrl)
+    .then((res) => res.json())
+    .then((data) => addImageElements(data.message))
+    .catch((err) => console.error('HERE IS THE ERROR', err)); 
+
+fetch(breedUrl)
+    .then((res) => res.json())
+    .then(data => addBreedListItems(data.message));
+
+function addImageElements(imgArr) {
+    imgArr.forEach(addImage);
+}
+
+function addImage(imgUrl) {
+    const dogImg = document.createElement('img');
+    dogImg.src = imgUrl;
+    imgDiv.append(dogImg);
+}
+
+function addBreedListItems(items) {
+    const itemKeys = Object.keys(items);
+
+    itemKeys.forEach((key) => { 
+    const breedLi = document.createElement('li');
+    breedLi.innerText = key;
+    breedUl.append(breedLi);
+});
+}
